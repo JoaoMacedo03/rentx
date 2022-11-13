76562b9b237f1ad2a62072f20601e9602ffab208
Joao Macedo <joaovictormacedo91@gmail.com>
2022-11-13 12:05:44 -0300

    test: test cases to use case create car

---
+++ src/modules/cars/useCases/createCar/CreateCarUseCase.spec.ts
@@ -0,0 +1,44 @@
+import { CarsRepositoryMock } from '@modules/cars/repositories/mocks/CarsRepositoryMock';
+import { AppError } from '@shared/errors/AppError';
+
+import { CreateCarUseCase } from './CreateCarUseCase';
+
+let createCarUseCase: CreateCarUseCase;
+let carsRepositoryMock: CarsRepositoryMock;
+let carMock;
+
+describe('Create Car', () => {
+  beforeEach(() => {
+    carMock = {
+      name: 'Name Car',
+      description: 'Description Car',
+      daily_rate: 100,
+      license_plate: 'ABC-1234',
+      fine_amount: 60,
+      brand: 'Brand',
+      category_id: 'Category',
+    };
+    carsRepositoryMock = new CarsRepositoryMock();
+    createCarUseCase = new CreateCarUseCase(carsRepositoryMock);
+  });
+
+  it('Should be able to create a new car', async () => {
+    const car = await createCarUseCase.execute(carMock);
+    expect(car).toHaveProperty('id');
+  });
+
+  it('Should not be able to create a car with the same license plate', () => {
+    expect(async () => {
+      await createCarUseCase.execute(carMock);
+
+      carMock.name = 'Car 2';
+
+      await createCarUseCase.execute(carMock);
+    }).rejects.toBeInstanceOf(AppError);
+  });
+
+  it('Should create a car with available true by default', async () => {
+    const car = await createCarUseCase.execute(carMock);
+    expect(car.available).toBe(true);
+  });
+});
