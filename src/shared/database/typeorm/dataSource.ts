da2a420e813febe024bea764272a9ae003bb6694
Joao Macedo <joaovictormacedo91@gmail.com>
2022-11-13 12:14:19 -0300

    fix: getting the ormconfig from the config path

--- src/shared/database/typeorm/dataSource.ts
+++ src/shared/database/typeorm/dataSource.ts
@@ -2,6 +2,6 @@ import 'dotenv/config';
 import 'reflect-metadata';
 import { DataSource, DataSourceOptions } from 'typeorm';
 
-import ormConfig from '../../../../ormconfig.json';
+import ormConfig from '@config/ormconfig.json';
 
 export const dataSource = new DataSource(ormConfig as DataSourceOptions);
