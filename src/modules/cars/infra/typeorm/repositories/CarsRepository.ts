60046bdde3682907d28fe85332de99c62cb6fa79
Joao Macedo <joaovictormacedo91@gmail.com>
2022-11-13 12:01:20 -0300

    feat: adding the car repository

---
+++ src/modules/cars/infra/typeorm/repositories/CarsRepository.ts
@@ -0,0 +1,51 @@
+import { Repository } from 'typeorm';
+
+import { ICreateCarDTO } from '@modules/cars/dtos/ICreateCarDTO';
+import { IListCarsDTO } from '@modules/cars/dtos/IListCarsDTO';
+import { ICarsRepository } from '@modules/cars/repositories/contracts/ICarsRepository';
+import { dataSource } from '@shared/database/typeorm/dataSource';
+
+import { Car } from '../entities/Car';
+
+class CarsRepository implements ICarsRepository {
+  private repository: Repository<Car>;
+
+  constructor() {
+    this.repository = dataSource.getRepository(Car);
+  }
+
+  async create(data: ICreateCarDTO): Promise<Car> {
+    const car = this.repository.create(data);
+    await this.repository.save(car);
+    return car;
+  }
+
+  async findByLicensePlate(license_plate: string): Promise<Car> {
+    const car = await this.repository.findOneBy({ license_plate });
+    return car;
+  }
+
+  async findAllAvailable({
+    brand,
+    category_id,
+    name,
+  }: IListCarsDTO): Promise<Car[]> {
+    const carsQuery = this.repository
+      .createQueryBuilder('c')
+      .where('available = :available', { available: true });
+
+    if (brand) carsQuery.andWhere('c.brand = :brand', { brand });
+    if (name) carsQuery.andWhere('c.name = :name', { name });
+    if (category_id)
+      carsQuery.andWhere('c.category_id = :category_id', { category_id });
+
+    const cars = await carsQuery.getMany();
+    return cars;
+  }
+
+  async findById(id: string): Promise<Car> {
+    return this.repository.findOneBy({ id });
+  }
+}
+
+export { CarsRepository };
