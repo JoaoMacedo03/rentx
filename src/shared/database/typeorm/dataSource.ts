fcfbd62bd392d3c1945bef307afab72f6fd54c07
Joao Macedo <joaovictormacedo91@gmail.com>
2022-11-15 18:19:44 -0300

    feat: creating the database folder inside shared

--- src/shared/database/typeorm/dataSource.ts
+++ src/shared/database/typeorm/dataSource.ts
@@ -4,4 +4,8 @@ import { DataSource, DataSourceOptions } from 'typeorm';
 
 import ormConfig from '@config/ormconfig.json';
 
+ormConfig.database = process.env.NODE_ENV === 'test' ? 'rentx_test' : 'rentx';
+ormConfig.host =
+  process.env.NODE_ENV === 'test' ? 'localhost' : 'rentx_database';
+
 export const dataSource = new DataSource(ormConfig as DataSourceOptions);
