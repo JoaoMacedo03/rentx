e118019c379ef51ec34776a9a485ba95066141a6
Joao Macedo <joaovictormacedo91@gmail.com>
2022-11-15 18:19:18 -0300

    feat: creating the infra folder

--- src/shared/infra/http/middlewares/ensureAuthenticated.ts
+++ src/shared/infra/http/middlewares/ensureAuthenticated.ts
@@ -1,7 +1,7 @@
 import { NextFunction, Request, Response } from 'express';
 import { verify } from 'jsonwebtoken';
 
-import { UsersRepository } from '@modules/accounts/infra/typeorm/repositories/UsersRepository';
+import auth from '@config/auth';
 import { AppError } from '@shared/errors/AppError';
 
 interface IPayload {
@@ -19,15 +19,7 @@ export async function ensureAuthenticated(
   const [, token] = authHeader.split(' ');
 
   try {
-    const { sub: userId } = verify(
-      token,
-      '3a6788c8d90070a1209fbcc400ae3b90',
-    ) as IPayload;
-
-    const usersRepository = new UsersRepository();
-
-    const user = await usersRepository.findById(userId);
-    if (!user) throw new AppError('User does not exists', 401);
+    const { sub: userId } = verify(token, auth.secret_token) as IPayload;
 
     request.user = {
       id: userId,
--- src/shared/infra/http/server.ts
+++ src/shared/infra/http/server.ts
@@ -3,22 +3,28 @@ import express from 'express';
 import 'express-async-errors';
 import swaggerUI from 'swagger-ui-express';
 
+import upload from '@config/upload';
 import { dataSource } from '@shared/database/typeorm/dataSource';
 import { router } from '@shared/infra/http/routes';
 
+import 'dotenv/config';
 import '@shared/container';
+
 import swaggerFile from '../../../doc/swagger.json';
 import { appErrorResponse } from './middlewares/appErrorResponse';
+
+const app = express();
 
 dataSource
   .initialize()
   .then(() => {
     console.log('💥️ Database initialized');
-    const app = express();
-
     app.use(express.json());
 
     app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(swaggerFile));
+
+    app.use('/avatar', express.static(`${upload.tmpFolder}/avatar`));
+    app.use('/cars', express.static(`${upload.tmpFolder}/cars`));
 
     app.use(router);
 
@@ -27,5 +33,7 @@ dataSource
     app.listen(3333, () => console.log('😈️ Server is running!'));
   })
   .catch(err => {
-    console.log('Error Database -> ', err);
+    console.log('Error Database -> ', err.message);
   });
+
+export { app };
