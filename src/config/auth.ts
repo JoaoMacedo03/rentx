5db591c40389df533711ad847358ed7b940c1cfb
Joao Macedo <joaovictormacedo91@gmail.com>
2022-11-15 18:15:32 -0300

    feat: adding the authentication config

---
+++ src/config/auth.ts
@@ -0,0 +1,7 @@
+export default Object.freeze({
+  secret_token: '3a6788c8d90070a1209fbcc400ae3b90',
+  secret_refresh_token: 'c22ae04cfd40d3d80e1b8376a59286bc',
+  expires_in_token: '15m',
+  expires_in_refresh_token: '30d',
+  expires_refresh_token_days: 30,
+});
